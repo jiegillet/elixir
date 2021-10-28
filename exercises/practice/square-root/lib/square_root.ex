a0d3c51a42f585b34f0700be3ded3829b9992263
Jérémie Gillet <jie.gillet@gmail.com>
2021-10-28 21:24:52 +0900

    add empty file

--- exercises/practice/square-root/lib/square_root.ex
+++ exercises/practice/square-root/lib/square_root.ex
@@ -1,8 +1,30 @@
 defmodule SquareRoot do
-  @doc """
-  Calculate the integer square root of a positive integer
-  """
-  @spec calculate(radicand :: pos_integer) :: pos_integer
+  defguardp is_positive(n) when n >= 0
+
+  defmacro unless_equal_to(guess, goal, do: expr) do
+    quote do
+      if unquote(guess) == unquote(goal) do
+        unquote(goal)
+      else
+        unquote(expr)
+      end
+    end
+  end
+
+  def calculate(1), do: 1
+
   def calculate(radicand) do
+    guess = div(radicand, 2)
+    do_calculate(radicand, guess)
+  end
+
+  defp do_calculate(radicand, guess) when is_positive(guess) do
+    new_guess =
+      div(guess + div(radicand, guess), 2)
+      |> IO.inspect()
+
+    unless_equal_to new_guess, guess do
+      do_calculate(radicand, new_guess)
+    end
   end
 end
